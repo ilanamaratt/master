551b09eaf47e0f46312356ba229c8daba9f87010
ilanamaratt <azikona0206@gmail.com>
2021-03-14 14:05:50 +0600

    2 files modified, 16 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { AboutComponent } from "./about/about.component";
+import { AlbumsComponent } from "./albums/albums.component";
+import { AlbumDetailsComponent } from "./album-details/album-details.component";
+import { AlbumPhotosComponent } from "./album-photos/album-photos.component";
+import { HomeComponent } from "./home/home.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AlbumsComponent,
+    AlbumDetailsComponent,
+    AlbumPhotosComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
